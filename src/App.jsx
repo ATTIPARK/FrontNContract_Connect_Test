4a4d13294bf42e284c2ed759814371df6e79b92b
ATTIPARK <qkrqjawls0922@gmail.com>
2023-07-02 16:28:04 +0900

    finish connecting

--- src/App.jsx
+++ src/App.jsx
@@ -5,157 +5,209 @@ import tokenabi from './abi_token.json';
 import nftabi from './abi_nft.json';
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState("");
+  const [token, setToken] = useState("");
+  const [present, setPresent] = useState(false);
+  const [currentB, setCurrentB] = useState("");
+  const [finalB, setFinalB] = useState("");
+  const [ratio, setRatio] = useState("");
+  const [tokenURI, setTokenURI] = useState("");
 
-  const [blockNumber, setBlockNumber] = useState();
-  const [balance, setBalance] = useState();
-  const [account, setAccount] = useState();
-  const [chainId, setChainId] = useState();
-  const [tbalance, setTbalance] = useState();
+  const web3 = new Web3(window.ethereum);
+  // const web3_2 = new Web3("https://goerli.infura.io/v3/13bee00ed8d04b969173e4691bfe996c");
 
-  const web3 = new Web3("wss://goerli.infura.io/ws/v3/13bee00ed8d04b969173e4691bfe996c");
-  const web3_2 = new Web3("https://goerli.infura.io/v3/13bee00ed8d04b969173e4691bfe996c");
+  // const web3 = new Web3(window.ethereum);
 
-  var token_c_address = '0x8Bc205608A8424fC9bF0975cB8518DefBa4C3766';
-  var nft_c_address = '0x41B90AD08487Cb7Bcf7e60E4e1845A54b1A4b22d';
+  var token_c_address = '0xCbC2CbdeFcAc44199C3F369A3e0153c78C47d1fC';
+  var nft_c_address = '0x703b2629698Fd3DFa591cA2b00f647a2C4F921A2';
 
-  var tokenContract = new web3_2.eth.Contract(tokenabi, token_c_address);
-  var nftContract = new web3_2.eth.Contract(nftabi, nft_c_address);
+  var tokenContract = new web3.eth.Contract(tokenabi, token_c_address);
+  var nftContract = new web3.eth.Contract(nftabi, nft_c_address);
 
-  async function connect() {
-    if(window.ethereum) {
-      try {
-        const res = await window.ethereum.request({
-          method : "eth_requestAccounts",
-        });
-        setAccount(res[0]);
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
 
-        getBalance()
-        getTbalance();
-      
-      } catch(err) {
-        console.error(err);
-      }
-    } else {
-      console.log("Install metamask");
+      console.log(accounts[0]);
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
-  async function getTbalance() {
-    if(account) {
-      try {
-        var a = await tokenContract.methods.balanceOf(account).call()
-        setTbalance(Number(a));
-      } catch(err) {
-        console.error(err);
-      }
-    } else {
-      console.log("connect the wallet");
-    }
-  }
+  const onClickLogOut = () => {
+    setAccount("");
+  };
 
-  useEffect(()=>{
-    async function getBlock() {
-      const blockNumber = await web3.eth.getBlockNumber();
-      setBlockNumber(Number(blockNumber));
-    }
-
-    getBlock();
-  })
-  
-  
-  async function getBalance() {
+  const onClickBalance = async () => {
     const res = await window.ethereum.request({
       method : "eth_requestAccounts",
     });
 
-    if(account) {
+    try {
+      if (!account) return;
+
       const _balance = await window.ethereum.request({
         method : "eth_getBalance",
         params : [res[0].toString(), "latest"]
       });
     
+      console.log(_balance);
       setBalance(ethers.formatEther(_balance));
-    } else {
-      console.log("wallet is not connected");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onClickToken = async () => {
+    try {
+      const _token = await tokenContract.methods.balanceOf(account).call();
+
+      console.log(_token);
+      setToken(Number(_token));
+    } catch(error) {
+      console.error(error);
     }
   }
 
-  useEffect(()=> {
-
-    async function subscribeBlock() {
-      const subscription = await web3.eth.subscribe("newHeads");
-      subscription.on("data", async(blockHead) => {
-        setBlockNumber(Number(blockHead.number));
-      });
-    }
-
-    subscribeBlock();
-
-    getTbalance();
-    getBalance();
-  })
-
-  async function getChainId() {
-    if(window.ethereum) {
-      const ID = await window.ethereum.request({
-        method : "eth_chainId",
-      });
-      setChainId(ID);
-    }
-  }
-
-  getChainId();
-
-  async function chainChanged() {
-    if(window.ethereum) {
-      setAccount(null);
-      setBalance(null);
-      connect();
-      getChainId();
-    }
-  }
-
-  useEffect(()=> {
-    if(window.ethereum) {
-      window.ethereum.on("chainChanged", chainChanged)
-    }
-  })
-
-  useEffect(()=> {
-    if(window.ethereum) {
-      window.ethereum.on("accountsChanged", connect)
-    }
-  })
-
-  async function mintNFTwithERC20(e) {
+  const buyToken = async(e) => {
     e.preventDefault();
     const data = new FormData(e.target);
-    
-    await window.ethereum.request({
-      method : "eth_sendTransaction",
-      params : [{
-        from : account, 
-        to : nft_c_address,
-        // value : ,
-        data : nftContract.methods.mintNFT1(data.get("Number", "Price")).encodeABI()}]
-    })
+    // var input = web3.utils.numberToHex(Number(data.get("amount")));
+
+    // await window.ethereum.request({
+    //   method : "eth_sendTransaction",
+    //   params : [{from : account, data : nftContract.methods.buyCoin(data.get("amount")).encodeABI()}]
+    // })
+
+    await nftContract.methods.buyCoin(data.get("amount")).send({
+      from: account,
+    });
+  }
+
+  // const getName = async () => {
+  //   try {
+  //     const _name = await tokenContract.methods.name().call();
+
+  //     console.log(name);
+  //     setName(_name);
+  //   } catch(error) {
+  //     console.error(error);
+  //   }
+  // }
+  // getName();
+
+  const submitPresent = async(e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    await nftContract.methods.mintNFT1(data.get("number_1"), data.get("price_1")).send({
+      from: account,
+    });
+  }
+
+  const showPresent = async(e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    const result = await nftContract.methods.PRESENT(data.get("who_2"), data.get("number_2")).call();
+    const result2 = await nftContract.methods.getChargeRatio(data.get("who_2"), data.get("number_2")).call();
+    // console.log(result);
+    // console.log(result2);
+    setPresent(true);
+    setCurrentB(Number(result.currentB));
+    setFinalB(Number(result.finalB));
+    setRatio(Number(result2));
+  }
+
+  const chargePresent = async(e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    await nftContract.methods.chargeBalance(data.get("who_3"), data.get("number_3"), data.get("price_3")).send({
+      from: account,
+    });
+  }
+
+  const getTokenURI = async(e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    const result = await nftContract.methods.tokenURI(data.get("tokenID_4")).call();
+
+    setTokenURI(result);
   }
 
   return (
-    <div className="bg-pink-300 min-h-screen flex justify-center items-center">
-      <div className="border-4 border-black mr-10" onClick={()=>{connect();}}>CONNECT WALLET</div>
-      <ul className="flex-row">
-        <li>current Block number : {blockNumber}</li>
-        <li>current Address : {account}</li>
-        <li>currnet balance : {balance} eth</li>
-        <li>current token balance : {tbalance} </li>
-        <li>current chainId : {chainId} </li>
-      </ul>
-      <form className="border-2" onSubmit={mintNFTwithERC20}>
-        <input type="text" name="Number"></input>
-        <input type="text" name="Price"></input>
-        <button type="submit">MINT_ERC20</button>
-      </form>
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {account ? (
+        <div>
+          <div className="text-main font-bold text-xl ">
+            {account.substring(0, 4)}...
+            {account.substring(account.length - 4)}
+            <button onClick={onClickLogOut} className="ml-4 btn-style">
+              로그아웃
+            </button>
+          </div>
+          <div className="flex items-center mt-4">
+            <button onClick={onClickBalance} className="btn-style mr-4">
+              잔액 조회
+            </button>
+            {balance && (
+              <div className="text-main font-bold text-xl">
+                {balance} goerli
+              </div>
+            )}
+          </div>
+          <form onSubmit={buyToken} className="mt-4">
+            <input type="text" name="amount" placeholder="구매할 양을 입력하게요"></input>
+            <button className="ml-4" type="submit">토큰 구매하기</button>
+          </form>
+          <div className="flex items-center mt-4">
+            <button onClick={onClickToken} className="btn-style mr-4">
+              토큰 조회
+            </button>
+            {token && (
+              <div className="text-main font-bold text-xl">
+                {token} CT
+              </div>
+            )}
+          </div>
+          <form onSubmit={submitPresent} className="mt-4">
+            <input type="text" name="number_1" placeholder="몇번쨰 선물"></input>
+            <input type="text" name="price_1" placeholder="선물의 금액"></input>
+            <button className="ml-4" type="submit">선물 등록하기</button>
+          </form>
+          <form onSubmit={showPresent} className="mt-4">
+            <input type="text" name="who_2" placeholder="누구의 선물"></input>
+            <input type="text" name="number_2" placeholder="몇번쨰 선물"></input>
+            <button className="ml-4" type="submit">선물 보기</button>
+            {present && (
+              <div className="text-main font-bold text-xl">
+                {currentB} / {finalB} ({ratio}%)
+              </div>
+            )}
+          </form>
+          <form onSubmit={chargePresent} className="mt-4">
+            <input type="text" name="who_3" placeholder="누구의 선물"></input>
+            <input type="text" name="number_3" placeholder="몇번쨰 선물"></input>
+            <input type="text" name="price_3" placeholder="선물의 금액"></input>
+            <button className="ml-4" type="submit">돈 충전하기</button>
+          </form>
+          <form onSubmit={getTokenURI} className="mt-4">
+            <input type="text" name="tokenID_4" placeholder="토큰 아이디"></input>
+            <button className="ml-4" type="submit">해당nft의 토큰uri 보기</button>
+            <div>{tokenURI}</div>
+          </form>
+        </div>
+      ) : (
+        <button onClick={onClickAccount} className="btn-style">
+          버튼
+        </button>
+      )}
     </div>
   );
 }
